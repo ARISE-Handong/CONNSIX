3049986c999c5a71a5f82456e4bed879c0d2248e
uja <21700287@handong.edu>
2021-10-13 20:19:10 +0900

    input_ai.py updated

--- example/input_ai.py
+++ example/input_ai.py
@@ -7,12 +7,7 @@ def make_move():
 def main():
 	ip = input("input ip: ")
 	port = int(input("input port number: "))
-	dummy_home = int(input("input 1 for black, 2 for white: "))
-	
-	if dummy_home == 1:
-		dummy_away = 2
-	elif dummy_home == 2:
-		dummy_away = 1
+	dummy_home = input("input BLACK or WHITE: ") 
 
 	red_stones = connsix.lets_connect(ip, port, dummy_home)
 	if len(red_stones):	
@@ -23,7 +18,7 @@ def main():
 		if user_in == "coor":
 			while user_in != "quit":
 				user_in = input("input a query (quit to quit): ")
-				print(connsix.query(user_in))
+				print(connsix.get_stone_at(user_in))
 		else:
 			away_move = connsix.draw_and_read(user_in)
 			print("Received away move from server: " + away_move)

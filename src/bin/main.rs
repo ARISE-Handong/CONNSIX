5b1b9548e35fcdc4d6456e8ba2fa07f1cedee739
Yeahn <21900151@handong.edu>
2021-11-12 19:19:12 +0900

    gui

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -16,20 +16,39 @@ use std::thread;
 use std::io::prelude::*;
 use std::time::Duration;
 
+use server::UserInterface;
+
 fn main() {
     // GUI 연결
+    let gui = UserInterface::new(String::from("172.20.10.2"), 8083);
+    
     // setting 받기
+    let red_stones = gui.pull();
+    println!("main red: {}", red_stones);
+    let black_port = gui.pull();
+    println!("main black port: {}", black_port);
+    let white_port = gui.pull();
+    println!("main white port: {}", white_port);
+    let interval = gui.pull();
+    println!("main interval: {}", interval);
+
+    // setting 정보 정리
+    
     // Player 연결
-    let (black_player, white_player) = server::connect_player(8081, 8082);
+    let ip = String::from("127.0.0.1");
+    let (black_player, white_player) = server::connect_player(ip, 8081, 8082);
 
     // GUI에 성공 메세지 보내기
+    gui.push(String::from("READY"));
+
     // GUI에서 시작 정보 받기
-    println!("Press any key to start the match...") ;
-	let mut c:[u8; 1] = [0; 1];
-    std::io::stdin().read(&mut c);
+
+    println!("gui hi from main?");
+    let start = gui.pull();
+    println!("start: {}\n", start);
 
     // 적돌 정보 보내기
-    let red_stones = String::from("A01");
+    // let red_stones = String::from("A01");
     black_player.push(red_stones.clone());
     white_player.push(red_stones.clone());
 
@@ -39,12 +58,18 @@ fn main() {
         // 돌 보내기
     loop {
         let from_black = black_player.pull();
-		println!("from_black: {}", &from_black) ;
-		white_player.push(from_black);
+		println!("main from_black: {}", &from_black);
+
+		gui.push(format!("B{}", from_black));
+		white_player.push(from_black.clone());
+		
+        let from_white = white_player.pull();
+		println!("main from_white: {}", &from_white) ;
+		
+        gui.push(format!("W{}", from_white.clone()));
+        black_player.push(from_white);
+
 		thread::sleep(Duration::from_secs(1)) ;
-		let from_white = white_player.pull();
-		println!("from_white: {}", &from_white) ;
-		black_player.push(from_white);
     }
     
 }
